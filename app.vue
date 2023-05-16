cc47c5b28e5cac9f45494915c0e88b02dd818f03
ZulRoman <133333789+ZulRoman@users.noreply.github.com>
2023-05-16 14:26:58 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -11,8 +11,8 @@
 <br>
 
 
-
-<div class="relative overflow-x-auto flex justify-center">
+<div class="flex gap-2 justify-center text-lg">
+<div class="relative overflow-x-auto flex">
     <table class="center">
         <thead class="bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -23,7 +23,7 @@
         </thead>
         <tbody>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center flex">
                     <ul class="list-decimal pl-6 text-left">
                         <li class="mb-2">Panci</li>
                         <li class="mb-2">Sendok dan Garpu</li>
@@ -37,7 +37,7 @@
 </div>
 <br>
 
-<div class="relative overflow-x-auto flex justify-center">
+<div class="relative overflow-x-auto">
     <table class="center">
         <thead class="bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -65,7 +65,7 @@
     </table>
       </div>
     </div>
-
+</div>
 
     
 <h2 class="text-2xl font-bold mb-4 text-center">Untuk selengkapnya bisa lihat video dibawah ini</h2>
@@ -75,6 +75,15 @@
     </iframe>
 </div>
       </div>
+<br>
+
+<h2 class="text-2xl font-bold mb-4 text-center">Produk Indomie</h2>
+
+<div class="flex justify-center">
+<card :image="'https://www.indomie.com/uploads/product/indomie-mi-goreng-special_thumb_094906798.png'" :title="'Indomie Goreng'" />
+<card :image="'https://www.indomie.com/uploads/product/indomie-mi-goreng-rendang-flavour_thumb_170333176.png'" :title="'Indomie Goreng Rendang'" />
+<card :image="'https://www.indomie.com/uploads/product/indomie-mi-goreng-cabe-ijo-flavour_thumb_145129182.png'" :title="'Indomie Goreng Cabe Ijo'" />
+</div>
 <br>
       <footer class="bg-gray-900 py-5 text-white text-center">
   <p>Copyright 2023 © Zulfikar Romansyah</p>
